d04df769e324da4dbaa4f0f0145e9c646d7938e6
77kkyu <77kkyu@gmail.com>
2021-10-13 14:57:15 +0900

    feat elasticsearch config

--- src/main/java/com/example/elkexamplespringboot/configuration/ElkConfig.java
+++ src/main/java/com/example/elkexamplespringboot/configuration/ElkConfig.java
@@ -1,2 +1,40 @@
-package com.example.elkexamplespringboot.configuration;public class ElkConfig {
+package com.example.elkexamplespringboot.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+@Slf4j
+@Configuration
+public class ElkConfig extends AbstractElasticsearchConfiguration {
+
+	@Value("${elasticsearch.host}")
+	private String host;
+
+	@Value("${elasticsearch.port}")
+	private int port;
+
+	@Bean
+	@Override
+	public RestHighLevelClient elasticsearchClient() {
+		final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+				.connectedTo(host + ":" + port)
+				.build();
+		return RestClients.create(clientConfiguration).rest();
+	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchOperations() {
+		return new ElasticsearchRestTemplate(elasticsearchClient());
+	}
+
 }
